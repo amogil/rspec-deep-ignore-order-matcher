ed0a4e49509a396b724b97b5fd9d346b4ee9d4b1
Alexey Mogilnikov <alexey@mogilnikov.name>
2013-03-18 17:37:35 +0600

    Method name fix

--- lib/rspec-deep-ignore-order-matcher.rb
+++ lib/rspec-deep-ignore-order-matcher.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require 'rspec-deep-ignore-order-matcher/version'
 
 RSpec::Matchers.define :be_deep_equal do |expected|
-	match { |actual| m1 actual, expected }
+	match { |actual| m? actual, expected }
 
 	failure_message_for_should do |actual|
 		"expected that #{actual} would be deep equal with #{expected}"
@@ -16,7 +16,7 @@ RSpec::Matchers.define :be_deep_equal do |expected|
 		"be deep equal with #{expected}"
 	end
 
-	def m1(actual, expected)
+	def m?(actual, expected)
 		return arrays_matches?(actual, expected) if expected.is_a?(Array) && actual.is_a?(Array)
 		return hashes_matches?(actual, expected) if expected.is_a?(Hash) && actual.is_a?(Hash)
 		expected == actual
@@ -25,7 +25,7 @@ RSpec::Matchers.define :be_deep_equal do |expected|
 	def arrays_matches?(actual, expected)
 		exp = expected.clone
 		actual.each do |a|
-			index = exp.find_index { |e| m1 a, e }
+			index = exp.find_index { |e| m? a, e }
 			return false if index.nil?
 			exp.delete_at(index)
 		end
@@ -34,7 +34,7 @@ RSpec::Matchers.define :be_deep_equal do |expected|
 
 	def hashes_matches?(actual, expected)
 		return false unless actual.keys == expected.keys
-		actual.each { |key, value| return false unless m1 value, expected[key] }
+		actual.each { |key, value| return false unless m? value, expected[key] }
 		true
 	end
 end
